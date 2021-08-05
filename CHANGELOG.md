bbca24c1dad2c60e1f2d827b4f35deb6e38eef59
SilentChaos512 <sworkman256@gmail.com>
2021-08-04 22:31:07 -0500

    Fix potion registry crash with Forge 37.0.24

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+### Fixed
+- Crash with Forge 37.0.24 (updating Forge _not_ required)
+
 ## [1.1.0] - 2021-08-01
 - Updated for Minecraft 1.17.1
 
--- src/main/java/net/silentchaos512/tokenenchanter/setup/Registration.java
+++ src/main/java/net/silentchaos512/tokenenchanter/setup/Registration.java
@@ -1,41 +1,24 @@
 package net.silentchaos512.tokenenchanter.setup;
 
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.crafting.RecipeSerializer;
-import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 import net.silentchaos512.tokenenchanter.TokenMod;
 
-import java.util.Collection;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
 public class Registration {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = create(ForgeRegistries.BLOCK_ENTITIES);
     public static final DeferredRegister<Block> BLOCKS = create(ForgeRegistries.BLOCKS);
     public static final DeferredRegister<MenuType<?>> CONTAINERS = create(ForgeRegistries.CONTAINERS);
-    public static final DeferredRegister<MobEffect> EFFECTS = create(ForgeRegistries.POTIONS);
-    public static final DeferredRegister<Enchantment> ENCHANTMENTS = create(ForgeRegistries.ENCHANTMENTS);
-    public static final DeferredRegister<EntityType<?>> ENTITIES = create(ForgeRegistries.ENTITIES);
     public static final DeferredRegister<Item> ITEMS = create(ForgeRegistries.ITEMS);
-    public static final DeferredRegister<GlobalLootModifierSerializer<?>> LOOT_MODIFIERS = create(ForgeRegistries.LOOT_MODIFIER_SERIALIZERS);
-    public static final DeferredRegister<Potion> POTIONS = create(ForgeRegistries.POTION_TYPES);
     public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = create(ForgeRegistries.RECIPE_SERIALIZERS);
-    public static final DeferredRegister<SoundEvent> SOUND_EVENTS = create(ForgeRegistries.SOUND_EVENTS);
 
     private Registration() {}
 
@@ -44,14 +27,8 @@ public class Registration {
         BLOCK_ENTITIES.register(modEventBus);
         BLOCKS.register(modEventBus);
         CONTAINERS.register(modEventBus);
-        EFFECTS.register(modEventBus);
-        ENCHANTMENTS.register(modEventBus);
-        ENTITIES.register(modEventBus);
         ITEMS.register(modEventBus);
-        LOOT_MODIFIERS.register(modEventBus);
-        POTIONS.register(modEventBus);
         RECIPE_SERIALIZERS.register(modEventBus);
-        SOUND_EVENTS.register(modEventBus);
 
         ModBlockEntityTypes.register();
         ModBlocks.register();
@@ -59,31 +36,6 @@ public class Registration {
         ModItems.register();
         ModLoot.register();
         ModRecipes.register();
-    }
-
-    @SuppressWarnings("unchecked")
-    public static <T extends Block> Collection<T> getBlocks(Class<T> clazz) {
-        return BLOCKS.getEntries().stream()
-                .map(RegistryObject::get)
-                .filter(clazz::isInstance)
-                .map(b -> (T) b)
-                .collect(Collectors.toList());
-    }
-
-    @SuppressWarnings("unchecked")
-    public static <T extends Item> Collection<T> getItems(Class<T> clazz) {
-        return ITEMS.getEntries().stream()
-                .map(RegistryObject::get)
-                .filter(clazz::isInstance)
-                .map(i -> (T) i)
-                .collect(Collectors.toList());
-    }
-
-    public static Collection<Item> getItems(Predicate<Item> predicate) {
-        return ITEMS.getEntries().stream()
-                .map(RegistryObject::get)
-                .filter(predicate)
-                .collect(Collectors.toList());
     }
 
     static <T extends IForgeRegistryEntry<T>> DeferredRegister<T> create(IForgeRegistry<T> registry) {
